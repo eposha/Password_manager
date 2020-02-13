3992a16dfbe25f77ed60e6620491c28040dbadf2
eposha <freedoctime@gmail.com>
2020-02-13 19:16:57 +0300

    basename

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = (env, argv) => {
         output: { // in which folder and file will be saving production version
             path: __dirname + '/pages',
             filename: "bundle.js",
-            publicPath: '/'
+            // publicPath: '/'
         },
         module: {
             rules: [ // for which files what use
